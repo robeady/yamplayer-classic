6429250e625bd4431c715ad4cad141127c40aad5
robeady <robertj.eady@gmail.com>
2019-07-02 00:28:44 +0100

    tidy up, explore source api

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,30 +1,38 @@
 use std::env;
-use std::io;
+use std::fs::File;
+use rodio::{Source, Sink};
+use std::error::Error;
+use std::io::{self, Read, Cursor};
 
-fn main() -> Result<(), Box<dyn Error>> {
+type Try<T> = Result<T, Box<dyn Error>>;
+
+fn main() -> Try<()> {
     let path_arg = env::args().nth(1).ok_or("first argument: file to play")?;
     play(path_arg)?;
-
     Ok(())
 }
 
-use std::fs::File;
-use std::io::BufReader;
-use rodio::Sink;
-use std::error::Error;
-use std::io::Read;
-use std::io::Cursor;
-
-fn play(path: String) -> Result<(), Box<dyn Error>> {
+fn play(path: String) -> Try<()> {
     let device = rodio::default_output_device().ok_or("no output device")?;
     let sink = Sink::new(&device);
+    let buffer = load_file(path)?;
+    let source = rodio::Decoder::new(Cursor::new(buffer))?;
+    let duration_secs = source.total_duration().expect("unknown duration").as_secs();
+    println!("track length: {}:{}", duration_secs / 60, duration_secs % 60);
+    sink.append(source);
+    sink.play();
+    wait_for_enter()?;
+    Ok(())
+}
+
+fn wait_for_enter() -> Try<()> {
+    io::stdin().read_line(&mut String::new())?;
+    Ok(())
+}
+
+fn load_file(path: String) -> Try<Vec<u8>> {
     let mut file = File::open(path)?;
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer)?;
-    let source = rodio::Decoder::new(Cursor::new(buffer))?;
-    // rodio::play_raw(&device, source.convert_samples());
-    sink.append(source);
-    sink.play();
-    io::stdin().read_line(&mut String::new())?;
-    Ok(())
+    Ok(buffer)
 }
