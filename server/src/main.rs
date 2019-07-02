88ce84bbecc02d2d36470003a582181e1ba1f508
robeady <robertj.eady@gmail.com>
2019-07-02 02:29:15 +0100

    now a server!

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,27 +1,74 @@
+use actix_web::{post, HttpResponse};
+use actix_web::{web, App, HttpServer, Responder};
+use rodio::{Device, Sink, Source};
+use serde_derive::{Deserialize, Serialize};
 use std::env;
-use std::fs::File;
-use rodio::{Source, Sink};
 use std::error::Error;
-use std::io::{self, Read, Cursor};
+use std::fs::File;
+use std::io::{self, Cursor, Read};
+use std::sync::Mutex;
 
 type Try<T> = Result<T, Box<dyn Error>>;
 
-fn main() -> Try<()> {
+fn main_cmd() -> Try<()> {
     let path_arg = env::args().nth(1).ok_or("first argument: file to play")?;
-    play(path_arg)?;
+    let mut app = PlayerApp::new()?;
+    app.play_file(&path_arg)?;
+    wait_for_enter()?;
     Ok(())
 }
 
-fn play(path: String) -> Try<()> {
-    let device = rodio::default_output_device().ok_or("no output device")?;
-    let sink = Sink::new(&device);
-    let buffer = load_file(path)?;
-    let source = rodio::Decoder::new(Cursor::new(buffer))?;
-    let duration_secs = source.total_duration().expect("unknown duration").as_secs();
-    println!("track length: {}:{}", duration_secs / 60, duration_secs % 60);
-    sink.append(source);
-    sink.play();
-    wait_for_enter()?;
+fn main() -> Try<()> {
+    run_server()
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct PlayRequest {
+    path: String,
+}
+
+struct PlayerApp {
+    device: Device,
+    sink: Option<Sink>,
+}
+
+impl PlayerApp {
+    fn new() -> Try<PlayerApp> {
+        let device = rodio::default_output_device().ok_or("no output device")?;
+        Ok(PlayerApp { device, sink: None })
+    }
+
+    fn play_file(&mut self, path: &str) -> Try<()> {
+        let buffer = load_file(path)?;
+        let source = rodio::Decoder::new(Cursor::new(buffer))?;
+        let duration_secs = source.total_duration().expect("unknown duration").as_secs();
+        println!(
+            "playing track with length: {}:{}",
+            duration_secs / 60,
+            duration_secs % 60
+        );
+        self.sink.as_ref().map(|s| s.stop());
+        let new_sink = Sink::new(&self.device);
+        new_sink.append(source);
+        new_sink.play();
+        self.sink = Some(new_sink);
+        Ok(())
+    }
+}
+
+#[post("/play")]
+fn play(state: web::Data<Mutex<PlayerApp>>, req: web::Json<PlayRequest>) -> HttpResponse {
+    let mut player = state.lock().unwrap();
+    player.play_file(&req.path).unwrap();
+    HttpResponse::Ok().finish()
+}
+
+fn run_server() -> Try<()> {
+    let player_app = PlayerApp::new()?;
+    let state = web::Data::new(Mutex::new(player_app));
+    HttpServer::new(move || App::new().register_data(state.clone()).service(play))
+        .bind("127.0.0.1:8080")?
+        .run()?;
     Ok(())
 }
 
@@ -30,7 +77,7 @@ fn wait_for_enter() -> Try<()> {
     Ok(())
 }
 
-fn load_file(path: String) -> Try<Vec<u8>> {
+fn load_file(path: &str) -> Try<Vec<u8>> {
     let mut file = File::open(path)?;
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer)?;
