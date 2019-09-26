97cc2a19e4d2887e671768f7556ed068552db045
Rob Eady <robertj.eady@gmail.com>
2019-09-26 22:40:48 +0100

    fmt

--- server/src/deezer_client.rs
+++ server/src/deezer_client.rs
@@ -10,15 +10,26 @@ pub struct DeezerClient {
 impl DeezerClient {
     pub fn new() -> Self {
         DeezerClient {
-            client: Client::new()
+            client: Client::new(),
         }
     }
 
     pub fn search(&self) -> String {
-        self.client.get(&[BASE_DEEZER_API, "search/track"].concat()).query(&[("q", "owl city")]).send().expect("Panic!!!").text().expect("More Panic!!!")
+        self.client
+            .get(&[BASE_DEEZER_API, "search/track"].concat())
+            .query(&[("q", "owl city")])
+            .send()
+            .expect("Panic!!!")
+            .text()
+            .expect("More Panic!!!")
     }
 
     pub fn track(&self) -> String {
-        self.client.get(&[BASE_DEEZER_API, "track/4188437"].concat()).send().expect("Panic!!!").text().expect("More Panic!!!")
+        self.client
+            .get(&[BASE_DEEZER_API, "track/4188437"].concat())
+            .send()
+            .expect("Panic!!!")
+            .text()
+            .expect("More Panic!!!")
     }
 }
